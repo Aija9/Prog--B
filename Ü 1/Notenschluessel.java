633e07758d66f7742552628b6d4c611dfb8db84c
Mo-Abb <67332936+Mo-Abb@users.noreply.github.com>
2020-11-24 11:16:36 +0100

    Update Notenschluessel.java

--- Ü 1/Notenschluessel.java
+++ Ü 1/Notenschluessel.java
@@ -1,30 +1,22 @@
 public class Notenschluessel {
     public static void main(String[] args) {
-		int Note ;
-		double Punkte = 34.5 ;
-		if (Punkte <20){
-			Note = 50;
-		} else if (Punkte < 22){
-			Note = 40;
-		} else if (Punkte < 24){
-			Note = 37;
-		} else if (Punkte < 26){ 
-			Note = 33;
-		} else if (Punkte < 28){
-			Note = 30;
-		} else if (Punkte < 30){
-			Note = 27;
-		} else if (Punkte < 32){
-			Note = 23;
-		} else if (Punkte < 34){
-			Note = 20;
-		} else if (Punkte < 36){
-			Note = 17;
-		} else if (Punkte < 36){
-			Note = 13;
-		}	else {
-			Note = 10;
-		}	
-		System.out.println(Note);
+	float grade = 17f;
+	float pointsMin;
+	float pointsMax;
+	
+	if (grade==50){pointsMin = 0f; pointsMax = 19.5f;}
+	else if (grade==40){pointsMin = 20f; pointsMax = 21.5f;}
+	else if (grade==37){pointsMin = 22f; pointsMax = 23.5f;}
+	else if (grade==33){pointsMin = 24f; pointsMax = 25.5f;}
+	else if (grade==30){pointsMin = 26f; pointsMax = 27.5f;}
+	else if (grade==27){pointsMin = 28f; pointsMax = 29.5f;}
+	else if (grade==23){pointsMin = 30f; pointsMax = 31.5f;}
+	else if (grade==20){pointsMin = 32f; pointsMax = 33.5f;}
+	else if (grade==17){pointsMin = 34f; pointsMax = 35.5f;}
+	else if (grade==13){pointsMin = 36f; pointsMax = 37.5f;}
+	else if (grade==10){pointsMin = 38f; pointsMax = 40.5f;}
+	else {pointsMin = 0f; pointsMax = 0f; System.out.println("Error");}
+	    
+	System.out.println(pointsMin+" "+pointsMax);
     }
 }
