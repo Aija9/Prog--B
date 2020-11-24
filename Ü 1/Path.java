cba2f6fe0ef2c8a6b748eca9900f99523f27c183
Mo-Abb <67332936+Mo-Abb@users.noreply.github.com>
2020-11-24 10:44:24 +0100

    Update Path.java

--- Ü 1/Path.java
+++ Ü 1/Path.java
@@ -1,5 +1,5 @@
 class Path {
     public static void main(String[] args) {
-        System.out.println("Path = C:\\Users\\Public"); 
+        System.out.println("Path = \"C:\\Users\\Public\""); 
     }
 }
