8d7e5ad38b6bbbdb4c370a71d719f42b41cd9048
Aija9 <51456402+Aija9@users.noreply.github.com>
2020-11-25 11:55:05 +0100

    Aufgabe F

---
+++ Division.java
@@ -0,0 +1,14 @@
+
+public class Division {
+	public static void main(String args[]) {
+		double a = 6;
+		double b = 3;
+
+		if (b == 0) {
+			System.out.println("NaN");
+		} else {
+			System.out.println((double) a / (double) b);
+		}
+	}
+
+}
